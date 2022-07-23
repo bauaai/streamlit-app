b55521dcb5486e1b5be1e0f9936b11e9cd7747a9
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2022-07-23 12:39:23 +0300

    about page

--- apps/about.py
+++ apps/about.py
@@ -9,7 +9,16 @@ def app():
     """
     About page app function
     """
-    st.title("eee")
+    st.markdown("<h1 style='text-align: center; '>HAKKINDA</h1>", unsafe_allow_html=True)
+
+    st.subheader("")
+
+    st.markdown(
+        """
+        Bağçeşehir öğrencileri tarafından tema'nın isteği doğrultusunda tema işbirliğiyle geliştirildi.
+""")
+
+
     st.info(
         """
         Sentinel-2 verilerinden yararlanarak geliştirilen bu uygulama orman yangınlarının
@@ -18,3 +27,22 @@ def app():
         TEMA işbirliğiyle hazırlandı..
 """
     )
+
+    st.subheader("Sorumluluk Reddi Beyanı")
+    st.markdown("""
+    Bu çalışma ve içerdiği sorgu / analiz sonuçları tahmini değerler içermektedir. Veriler / sonuçlar, herhangi bir arazi gözlemine
+    dayanmadan uydu görüntüleri üzerinden görüntü işleme yazılımlarıyla otomatik – yarı otomatik olarak elde edilmiş olduğundan,
+    arazi gözlemleri ile uyumsuzluk gösterebilir.
+    """)
+
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        st.subheader("Geliştirenler")
+        st.write(
+        """[Osman Faruk Bayram](https://github.com/osbm)  \n  [Efe Can Kırbıyık](https://github.com/EFCK)  \n  [Ahmet Bilal Barışman](https://github.com/qimenez)
+        """
+        )
+
+    with col2:
+        st.subheader("Katkı Verenler")
