8a732b1b3311384776c0d1696473cb8d1889c39c
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2022-07-23 15:13:58 +0300

    about final

--- apps/about.py
+++ apps/about.py
@@ -22,15 +22,7 @@ def app():
         haritalama kütüphaneleri kullanılarak işlenmiş ve [streamlit](https://streamlit.io) kullanılarak internet sitesi haline getirilmiştir.
 """)
 
-
-    st.info(
-        """
-        Sentinel-2 verilerinden yararlanarak geliştirilen bu uygulama orman yangınlarının
-        analiz edilmesi ve izlenmesi amacıyla [Osman](https://github.com/osbm),
-        [Efe](https://github.com/EFCK) ve [Bilal](https://github.com/qimenez) tarafından
-        TEMA işbirliğiyle hazırlandı..
-"""
-    )
+    st.subheader("")
 
     st.subheader("Sorumluluk Reddi Beyanı")
     st.markdown("""
@@ -39,16 +31,22 @@ def app():
     arazi gözlemleri ile uyumsuzluk gösterebilir.
     """)
 
+    st.subheader("")
 
+    st.subheader("Geliştirenler")
 
     col1, col2 = st.columns([1, 1])
 
     with col1:
-        st.subheader("Geliştirenler")
         st.write(
-        """[Osman Faruk Bayram](https://github.com/osbm)  \n  [Efe Can Kırbıyık](https://github.com/EFCK)  \n  [Ahmet Bilal Barışman](https://github.com/qimenez)
+        """[Osman Faruk BAYRAM](https://github.com/osbm)  \n[Efe Can KIRBIYIK](https://github.com/EFCK)  \nAhmet Bilal BARIŞMAN  \n Aleyna
+        KURT  \n[Beyza BAYRAK](https://github.com/beyzabayrakk)
         """
         )
 
     with col2:
-        st.subheader("Katkı Verenler")
+        st.write(
+        """
+        Alp Giray SAVRUM  \n  Edip Emre BODUR  \n Aleyna Benan AYDI  \n  Kubilay Kaan DELİKTAŞ  \n  Ecem AYDOĞAN
+        """
+        )
